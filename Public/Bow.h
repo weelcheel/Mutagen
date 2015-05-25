00d324c1c95e76aa912eddce1e5f1a3b30794516
belven <belven000@gmail.com>
2015-05-25 15:38:17 +0100

    Updated diagram images

---
+++ Public/Bow.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Weapon.h"
+#include "Bow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MUTAGEN_API ABow : public AWeapon
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
